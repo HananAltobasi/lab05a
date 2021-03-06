c8c2a5548b88034e85b64b722115845162a8ae17
hanan omar altobasi <hananaltobase96@gmail.com>
2021-03-06 16:21:27 +0200

    solve the second problem

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
 'use strict';
-/////////////////////////////////////
-/* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
-
-"The sum of 4 and 7 is 11."
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
-
-// Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b) { 
     let sum= a + b;
-    let con=('The sum of '+a+' and '+b+ ' is '+sum+ '.')
+    let con=('The sum of '+a+' and '+b+ ' is '+sum+ '.');
    let arr1=[sum,con]
     return arr1;
 }
-
-// Here is the test for sum(); uncomment it to run it
  testSum(4, 7);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-
-/////////////////////////////////////
-/* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
-"The product of 5 and 9 is 45."
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
-
-// Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(a, b) { 
+    let multiply= a * b;
+    let mul=('The product of '+a+' and '+b+ ' is '+multiply+ '.');
+    let arr2=[multiply,mul];
+    return arr2;
 }
-
-// Here is the test for multiply(); uncomment it to run it
- //testMultiply(5,9);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
